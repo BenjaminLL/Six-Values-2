c4303f718aad2f997c4fb3184b7f1d7433450360
Shibo(Johnny) Meng <shibomeng@yahoo.com>
2018-06-30 15:02:22 -0400

    button

---
+++ assets/js/game.js
@@ -0,0 +1,6 @@
+var honesty = $("#honesty");
+var trust = $("#trust");
+var respect = $("respect");
+var responsibility = $("responsibility");
+var fairness = $("fairness");
+var courage = $("courage");
